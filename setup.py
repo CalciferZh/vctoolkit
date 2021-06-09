a0498208d3a4fce14a71baf9239c99e71788b8a2
Yuxiao Zhou <yuxiao.zhou@outlook.com>
2021-06-09 18:43:58 +0800

    with dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 import setuptools
 
 setuptools.setup(
-  name = 'vctoolkit',
-  version = '0.1.5.36',
-  author = 'Yuxiao Zhou',
-  author_email = 'yuxiao.zhou@outlook.com',
-  description = 'A simple wrapper for commonly used tools in visual computing.',
-  long_description = 'A simple wrapper for commonly used tools in visual computing.',
-  url = 'https://github.com/CalciferZh/vctoolkit',
-  packages = setuptools.find_packages(),
-  classifiers = ["Programming Language :: Python :: 3"]
+  name='vctoolkit',
+  version='0.1.5.37',
+  author='Yuxiao Zhou',
+  author_email='yuxiao.zhou@outlook.com',
+  description='A simple wrapper for commonly used tools in visual computing.',
+  url='https://github.com/CalciferZh/vctoolkit',
+  packages=setuptools.find_packages(),
+  classifiers=["Programming Language :: Python :: 3"],
+  install_requires=[
+    'opencv-python', 'h5py', 'imageio', 'numpy', 'scipy',
+    'transforms3d', 'tqdm', 'matplotlib'
+  ]
 )
